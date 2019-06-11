a877ad8a3526a732c8163a13932e0d2be642c017
NebotXavi <46298544+Nebotxavi@users.noreply.github.com>
2019-06-11 20:19:08 +0100

    Update SQL Movie-Rating Query Exercises.sql

--- SQL Movie-Rating Query Exercises.sql
+++ SQL Movie-Rating Query Exercises.sql
@@ -7,7 +7,7 @@ WHERE director = 'Steven Spielberg';
 #2 Find all years that have a movie that received a rating of 4 or 5, and sort them in increasing order. 
 
 SELECT DISTINCT year
-FROM movie as m, rating as r
+FROM movie AS m, rating AS r
 WHERE m.mID = r.mID AND
       stars >= 4
 ORDER BY year;
@@ -21,14 +21,14 @@ WHERE mID NOT IN (SELECT mID FROM rating);
 #4 Some reviewers didn't provide a date with their rating. Find the names of all reviewers who have ratings with a NULL value for the date. 
 
 SELECT name
-FROM reviewer as rev, rating as r
+FROM reviewer AS rev, rating AS r
 WHERE rev.rID = r.rID AND
       ratingDate IS NULL;
 
 #5 Write a query to return the ratings data in a more readable format: reviewer name, movie title, stars, and ratingDate. Also, sort the data, first by reviewer name, then by movie title, and lastly by number of stars. 
 
 SELECT name, title, stars, ratingDate
-FROM reviewer as rev, movie as m, rating as r
+FROM reviewer AS rev, movie AS m, rating AS r
 WHERE rev.rID = r.rID AND
       m.mID = r.mID 
 ORDER BY name, title, stars;
@@ -36,7 +36,7 @@ ORDER BY name, title, stars;
 #6 For all cases where the same reviewer rated the same movie twice and gave it a higher rating the second time, return the reviewer's name and the title of the movie. 
 
 SELECT DISTINCT name, title
-FROM reviewer as rev, movie as m, rating as r1, rating as r2
+FROM reviewer AS rev, movie AS m, rating AS r1, rating AS r2
 WHERE rev.rID = r1.rID AND
       m.mID = r1.mID AND
       r1.rID = r2.rID AND
@@ -55,7 +55,7 @@ ORDER BY title;
 #8 For each movie, return the title and the 'rating spread', that is, the difference between highest and lowest ratings given to that movie. Sort by rating spread from highest to lowest, then by movie title. 
 
 SELECT title, max(stars) - min(stars) as str
-FROM movie as m, rating as r1
+FROM movie AS m, rating AS r1
 WHERE m.mID = r1.mID  
 GROUP BY title
 ORDER BY str DESC, title;
@@ -64,12 +64,12 @@ ORDER BY str DESC, title;
 
 SELECT AVG(avg_old.avg_rating) - AVG(avg_new.avg_rating)
 FROM (SELECT AVG(stars) as avg_rating
-      FROM rating as r, movie as m
+      FROM rating AS r, movie AS m
       WHERE r.mID = m.mID AND
             year < 1980
-      GROUP BY r.mID) as avg_old, 
-     (SELECT AVG(stars) as avg_rating
-      FROM rating as r, movie as m
+      GROUP BY r.mID) AS avg_old, 
+     (SELECT AVG(stars) AS avg_rating
+      FROM rating AS r, movie AS m
       WHERE r.mID = m.mID AND
             year > 1980
       GROUP BY r.mID) as avg_new;
